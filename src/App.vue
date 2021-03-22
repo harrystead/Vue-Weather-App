7ab43cd40755ad4149ed808d58f19c5540df5c28
Harry Stead <harrystead9@gmail.com>
2021-03-22 14:44:20 +0000

    info on page

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,31 @@
 <template>
-  <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''">
+  <div
+    id="app"
+    :class="
+      typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''
+    "
+  >
     <main>
       <div class="search-box">
-        <input 
-          type="text" 
-          class="search-bar" 
+        <input
+          type="text"
+          class="search-bar"
           placeholder="Search..."
           v-model="query"
           @keypress="fetchWeather"
         />
       </div>
-
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
+          <div class="location">
+            {{ weather.name }}, {{ weather.sys.country }}
+          </div>
           <div class="date">{{ dateBuilder() }}</div>
         </div>
+      </div>
+      <div class="weather-box">
+        <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
+        <div class="weather">{{ weather.weather[0].main }}</div>
       </div>
     </main>
   </div>

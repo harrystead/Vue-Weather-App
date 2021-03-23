67f0f1ebaee262f2a11029e17d16b6058e7739b5
Harry Stead <harrystead9@gmail.com>
2021-03-23 10:23:01 +0000

    both apis working

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,11 @@
 <template>
-  <div
-    id="app"
-    :class="
-      typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''
-    "
-  >
+  <div id="app">
     <main>
-      <div class="search-box">
+      <div class="bar">
         <input
           type="text"
           class="search-bar"
-          placeholder="Search..."
+          placeholder="Search a city..."
           v-model="query"
           @keypress="fetchWeather"
         />
@@ -18,43 +13,53 @@
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
           <div class="location">
+            <div class="weather">{{ weather.weather[0].main }}</div>
             {{ weather.name }}, {{ weather.sys.country }}
           </div>
           <div class="date">{{ dateBuilder() }}</div>
         </div>
       </div>
-      <div class="weather-box">
-        <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
-        <div class="weather">{{ weather.weather[0].main }}</div>
+      <div class="weather-box" v-if="typeof weather.main != 'undefined'">
+        <div class="temp">{{ Math.round(weather.main.temp) }}°</div>
       </div>
+      <FiveDay :weatherFive="weatherFive" />
     </main>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import FiveDay from "./components/FiveDay.vue";
 
 export default {
   name: "App",
+  components: {
+    FiveDay,
+  },
   data() {
     return {
       api_key: "2bbd84d695f75e90260a321f5b80b8b5",
       url_base: "https://api.openweathermap.org/data/2.5/",
+      url_five: "https://api.openweathermap.org/data/2.5/forecast?q=",
       query: "",
       weather: {},
+      weatherFive: {},
     };
   },
   methods: {
-    fetchWeather(e) {
+    async fetchWeather(e) {
       if (e.key == "Enter") {
-        axios
-          .get(
-            `${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`
-          )
-          .then((res) => {
-            return res.data;
-          })
-          .then(this.setResults);
+        try {
+          const response1 = await axios.get(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`);
+          this.weather = response1.data;
+          const response2 = await axios.get(
+            `${this.url_five}${this.query}&appid=${this.api_key}`
+          );
+          console.log(response2.data.list[0].visibility)
+          this.weatherFive = response2.data;
+        } catch (e) {
+          console.log(e);
+        }
       }
     },
     setResults(results) {
@@ -96,4 +101,26 @@ export default {
 </script>
 
 <style>
+.bar {
+  text-align: center;
+  margin-top: 8%;
+}
+
+input {
+  height: 33px;
+  width: 20%;
+  border-radius: 12px;
+  border: none;
+  padding-left: 8px;
+  box-shadow: 0 0 4pt 2pt #d3d3d3;
+}
+
+.location-box,
+.weather-box {
+  text-align: center;
+}
+.temp {
+  font-weight: bold;
+  font-size: 80px;
+}
 </style>
---
+++ src/components/FiveDay.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+    <h3>{{weatherFive.list[0].visibility}}</h3>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'FiveDay',
+    props: {
+        weatherFive: Object,
+    }
+}
+</script>

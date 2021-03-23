ea30403e67d688a1261e80ce81250a7ac4c53e7b
Harry Stead <harrystead9@gmail.com>
2021-03-23 13:41:36 +0000

    styling

--- src/components/FiveDay.vue
+++ src/components/FiveDay.vue
@@ -1,14 +1,30 @@
 <template>
-  <div class="five-container" :key="weather.visibility" v-for="weather in weatherFive">
-      <div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">{{weather.dt_txt.slice(0,10)}}</h5>
-    <h6 class="card-subtitle mb-2 text-muted">{{ Math.floor(weather.main.temp - 273.15)}}°C</h6>
-    <p class="card-text">humidity: {{weather.main.humidity}}% </p>
-    <p>wind-speed: {{weather.wind.speed}}mph</p>
-    <p>pressure: {{weather.main.pressure}}mb</p>
-  </div>
-</div>
+  <div className="card-group">
+    <div
+      class="five-container"
+      :key="weather.visibility"
+      v-for="weather in weatherFive"
+    >
+      <div class="card" style="width: 12rem">
+        <div class="card-body">
+          <h5 class="card-title">{{ weather.dt_txt.slice(0, 10) }}</h5>
+          <h6 class="card-subtitle mb-2">
+            {{ Math.floor(weather.main.temp - 273.15) }}°C
+          </h6>
+          <small class="text-muted"
+            >humidity: {{ weather.main.humidity }}%</small
+          >
+          <hr />
+          <small class="text-muted"
+            >wind-speed: {{ weather.wind.speed }}mph</small
+          >
+          <hr />
+          <small class="text-muted"
+            >pressure: {{ weather.main.pressure }}mb</small
+          >
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -20,3 +36,36 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.card-group {
+  text-align: center;
+  margin: auto;
+  position: absolute;
+  left: 18%;
+}
+
+.five-container {
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+}
+.card {
+  border-radius: 15px;
+  margin-right: 10px;
+  display: inline-block;
+  box-shadow: 0 0 2pt 0.2pt #d3d3d3;
+}
+hr {
+  margin: 0px;
+  margin-top: 5px;
+}
+.card-title {
+  font-size: 14px;
+}
+.card-subtitle {
+  font-size: 22px;
+  font-weight: bold;
+}
+</style>
